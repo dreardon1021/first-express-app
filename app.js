8ffa3f2cdfcdd84c0a6fb19a85b3e89ef594c858
Dan Reardon <dreardon1021@gmail.com>
2020-08-10 17:04:40 -0600

    Finish mod6lesson2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express');
 const bodyParser = require('body-parser');
-const adminRoutes = require('./routes/admin')
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const app = express();
@@ -9,7 +9,8 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
+console.log(adminData.products)
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,12 @@
-const path = require('path')
-const express = require('express')
+const path = require('path');
+const express = require('express');
 const router = express.Router();
+const rootDir = require('../util/path');
+const adminData = require('./admin')
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+  console.log(adminData.products)
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router
