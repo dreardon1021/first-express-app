23ac072daf1325049d53b3f74827939395d54f4f
Dan Reardon <dreardon1021@gmail.com>
2020-08-11 16:09:28 -0600

    Convert HTML to Pug and add style

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const shopRoutes = require('./routes/shop')
 
 const app = express();
 
-app.set();
+app.set('view engine', 'pug');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,7 +16,7 @@ console.log(adminData.products)
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, '/views', '/404-err.html'));
+  res.status(404).render('404-err')
 })
 
 app.listen(3000)
