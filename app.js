49ae14aa74bae6daa1b0318dce6b029b7a56d1e1
Dan Reardon <dreardon1021@gmail.com>
2020-08-18 15:49:59 -0600

    Adjust project to EJS

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
-const expressHbs = require('express-handlebars')
 
 const app = express();
 
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}))
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
