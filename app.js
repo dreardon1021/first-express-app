94152f1b9e49761c0141d2bc234a7b946f3d6d87
Dan Reardon <dreardon1021@gmail.com>
2020-08-24 12:02:40 -0600

    Add node datadog

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+//Data Dog
+var StatsD = require('hot-shots');
+var dogstatsd = new StatsD();
+
+// Increment a counter.
+dogstatsd.increment('page.views')
+
+
 const path = require('path')
 const express = require('express');
 const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 const errorController = require('./controllers/errors')
+
+
+
 
 const app = express();
 
