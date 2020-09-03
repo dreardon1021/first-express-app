2026aa03aabf024fb088fa15797c3aa0a4fd83a1
Dan Reardon <dreardon1021@gmail.com>
2020-09-03 12:43:14 -0600

    Adjust admin prodcut card

--- models/product.js
+++ models/product.js
@@ -17,11 +17,11 @@ const getProductsFromFile = callback => {
 }
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor (title, imageUrl, description, price) {
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
-    this.price = this.price;
+    this.price = price;
   }
 
   save() {
